2772dbd1883ae24beda9a229e4672cee9a3047fb
Shokoofeh Pourmehr <sh.pourmehr@gmail.com>
2017-09-01 00:06:47 -0700

    add first AI code for bot clean

---
+++ artificialIntelligence/botBuilding/botClean.cpp
@@ -0,0 +1,109 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+class Cell {
+public:
+    int row;
+    int col;
+    Cell (int r, int c) : row (r), col (c) {}
+    Cell () {}
+    int distance ( Cell ce ) {
+        return abs(ce.col - col) + abs(ce.row - row);
+    }
+    int distance( int r, int c) {
+        return abs(c - col) + abs(r - row);
+    }
+
+};
+
+bool valid (Cell c) {
+    return (c.row > 0 && c.col > 0 && c.row < 5 && c.col < 5);
+}
+
+Cell findClosestDirt (int posr, int posc, const vector < string > & b) {
+    Cell d(0, 0);
+    Cell result;
+    int mindis = 1000;
+
+    for (int r = 0; r < 5; r++) {
+        for (int c = 0; c < 5; c++) {
+            d.row = r;
+            d.col = c;
+            if (b[r][c] == 'd')
+            {
+                if (d.distance (posr, posc) < mindis) {
+                    mindis = d.distance (posr, posc);
+                    result = d;
+                }
+            }
+        }
+    }
+
+    return result;
+}
+
+
+inline void moveLeft () {
+    cout << "LEFT\n";
+}
+
+
+inline void moveRight () {
+    cout << "RIGHT\n";
+}
+
+
+inline void moveUp () {
+    cout << "UP\n";
+}
+
+
+inline void moveDown () {
+    cout << "DOWN\n";
+}
+
+inline void cleanCell () {
+    cout << "CLEAN\n";
+}
+
+
+void next_move (int posr, int posc,  vector <string> board) {
+    if (board[posr][posc] == 'd') cleanCell();
+
+    Cell cell = findClosestDirt (posr, posc, board);
+    int distr = posr - cell.row;
+    int mr = abs(distr);
+    int distc = posc - cell.col;
+    int mc = abs(distc);
+    if ( distr < 0) {
+        moveDown();
+        return;
+    }
+    if ( distr > 0) {
+        moveUp();
+        return;
+    }
+
+    if (distc < 0) {
+        moveRight();
+        return;
+    }
+
+    if (distc > 0) {
+        moveLeft();
+        return;
+    }
+}
+int main(void) {
+    int pos[2];
+    vector <string> board;
+    cin >> pos[0] >> pos[1];
+    for (int i = 0; i < 5; i++) {
+        string s; cin >> s;
+        board.push_back(s);
+    }
+    next_move(pos[0], pos[1], board);
+    return 0;
+}
+
